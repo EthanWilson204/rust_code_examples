5e3826843594233ec8ad6b7f5dc51a61fc596238
EthanWilson204 <ehwilson.business@gmail.com>
2024-04-24 19:08:05 -0400

    Made the non-functional example actionally nonfunctional

--- functional_rust.rs
+++ functional_rust.rs
@@ -4,10 +4,10 @@
 fn main() {
 
     //non-functional way of increasing the numbers of each value
-    let vec1 = vec![10, 20, 30, 40, 50];
+    let vec1: [i32; 5] = [10, 20, 30, 40, 50];
     let mut vec2: Vec<i32> = Vec::new();
 
-    for &num in vec1.iter() {
+    for &num in &vec1{
         vec2.push(num + 15);
     }
     println!("non functional: {:?}", vec2);
