f1d155a06d51d4f0de301203ec262043c551f3cc
EthanWilson204 <ehwilson.business@gmail.com>
2024-04-24 19:47:20 -0400

    made struct for rust

---
+++ oo_rust.rs
@@ -0,0 +1,11 @@
+//structs define the variables of our "Objects"
+struct Dog {
+
+    //variables used to define instances of dog
+    pub breed: String,      //pub keyword makes this a public variable (default is private)
+    pub lifespan: i32,
+    pub activity: String,
+
+}
+
+//impl (implements) gives us more functionality to handle Dog
