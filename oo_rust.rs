05044016f6507ef3e0c7e408e3715cd70ae592ce
Ethan Wilson <ehwilson.business@gmail.com>
2024-04-25 02:59:14 -0400

    new example

--- oo_rust.rs
+++ oo_rust.rs
@@ -4,15 +4,15 @@ pub struct Dog {
     //variables used to define instances of dog
     pub breed: String,      //pub keyword makes this a public variable (default is private)
     lifespan: i32,
-    activity: String,
+    toy: String,
 
 }
 
 //impl (implements) gives us more functionality to handle Dog
 impl Dog {
 
-    fn new(breed: String, lifespan: i32, activity: String) -> Dog {
-        Dog {breed, lifespan, activity}
+    fn new(breed: String, lifespan: i32, toy: String) -> Dog {
+        Dog {breed, lifespan, toy}
     }
 
     fn get_breed(&self) -> String {
@@ -21,13 +21,13 @@ impl Dog {
     fn get_lifespan(&self) -> i32 {
         self.lifespan
     }
-    fn get_activity(&self) -> String {
-        self.activity.clone()
+    fn get_toy(&self) -> String {
+        self.toy.clone()
     }
 
 }
 
 fn main() {
-    let goodboy = Dog::new("Golden Retriever".to_string(), 15, "Highly Active".to_string()); 
-    println!("Dog Breed: {}\nLifespan: {} years\nActivity Level: {}", goodboy.get_breed(), goodboy.get_lifespan(), goodboy.get_activity());
+    let goodboy = Dog::new("Husky".to_string(), 14, "Tennis Ball".to_string()); 
+    println!("Dog Breed: {}\nLifespan: {} years\nActivity Level: {}", goodboy.get_breed(), goodboy.get_lifespan(), goodboy.get_toy());
 }
